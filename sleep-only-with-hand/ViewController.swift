42dcf1281b1bd76bbd96a403c717731b45175845
changhyeon743 <changhyeon743@gmail.com>
2018-08-18 16:36:02 +0900

    initial init

---
+++ sleep-only-with-hand/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sleep-only-with-hand
+//
+//  Created by 이창현 on 2018. 8. 18..
+//  Copyright © 2018년 이창현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
